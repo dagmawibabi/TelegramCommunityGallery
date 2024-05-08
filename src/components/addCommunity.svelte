759624f353b2b9638578ea5b0bdfa8ebd8b24741
dagmawibabi <1babidagi@gmail.com>
2024-05-08 22:19:59 +0300

    mobile ver done v1

--- src/components/addCommunity.svelte
+++ src/components/addCommunity.svelte
@@ -128,76 +128,76 @@
         </div>    
     </Dialog.Trigger>
 
-    <Dialog.Content class="sm:max-w-[425px] bg-zinc-900 border-zinc-800 rounded-xl ">
+    <Dialog.Content class="h-[400px] md:h-fit overflow-scroll w-[350px] sm:max-w-[425px] bg-zinc-900 border-zinc-800 rounded-xl ">
         <!-- HEADER -->
-        <Dialog.Header>
-            <Dialog.Title class="text-2xl font-semibold"> Add Channel / Group </Dialog.Title>
-            <Dialog.Description class="text-zinc-300 text-lg">
+        <Dialog.Header class="text-left">
+            <Dialog.Title class="text-md font-semibold"> Add Channel / Group </Dialog.Title>
+            <Dialog.Description class="text-zinc-300 text-sm">
                 Add your community and click save when you're done!
             </Dialog.Description>
         </Dialog.Header>
         
         <!-- FORM -->
-        <div class="flex flex-col">
-            <label for="communityName" class="pt-3 pb-3 text-lg font-semibold"> Community Name </label>
+        <div class="flex flex-col pt-0">
+            <label for="communityName" class="pb-3 text-sm font-semibold"> Community Name </label>
             <input 
                 id="communityName"
-                class="bg-zinc-800 rounded-xl p-2 px-4 outline-none"
+                class="bg-zinc-800 rounded-xl p-1 pb-2 px-3 outline-none text-sm"
                 placeholder="eg: Pavel Durov"
                 autocomplete="off"
                 required
             />
-            <label for="communityLink" class="pt-5 pb-3 text-lg font-semibold"> Community Link </label>
+            <label for="communityLink" class="pt-3 pb-3 text-sm font-semibold"> Community Link </label>
             <input 
                 id="communityLink"
-                class="bg-zinc-800 rounded-xl p-2 px-4 outline-none"
+                class="bg-zinc-800 rounded-xl p-1 pb-2 px-3 outline-none text-sm"
                 placeholder="eg: @Durov"
                 autocomplete="off"
                 on:input={(e) => changeDuplicates(e)}
                 required
             />
             {#if foundDuplicateLink == true}
-                <span class="text-red-500 text-lg pt-2 pl-2"> Username is duplicate! </span>
+                <span class="text-red-500 text-sm pt-2 pl-2"> Username is duplicate! </span>
             {/if}
-            <label for="communityDescription" class="pt-5 pb-3 text-lg font-semibold"> Community Description </label>
+            <label for="communityDescription" class="pt-3 pb-3 text-sm font-semibold"> Community Description </label>
             <textarea 
                 id="communityDescription"
-                class="bg-zinc-800 rounded-xl p-2 px-4 outline-none"
+                class="bg-zinc-800 rounded-xl p-1 pb-2 px-3 outline-none text-sm"
                 placeholder="eg: Announcement channel of Telegram's CEO "
                 rows="4"
                 maxlength="1000"
                 required
             />
-            <label for="communityOwner" class="pt-5 pb-3 text-lg font-semibold"> Community Owner <span class="text-zinc-500">[Optional]</span> </label>
+            <label for="communityOwner" class="pt-3 pb-3 text-sm font-semibold"> Community Owner <span class="font-normal text-zinc-500"> [Optional]  </span> </label>
             <input 
                 id="communityOwner"
-                class="bg-zinc-800 rounded-xl p-2 px-4 outline-none"
+                class="bg-zinc-800 rounded-xl p-1 pb-2 px-3 outline-none text-sm"
                 placeholder="eg: @PavelDurov"
                 autocomplete="off"
             />
-            <label for="communityTypes" class="pt-5 pb-3 text-lg font-semibold"> Community Type </label>
+            <label for="communityTypes" class="pt-3 pb-2 text-sm font-semibold"> Community Type </label>
             <div class="flex" id="communityTypes">
                 <RadioComponent {options} fontSize={16} legend='' bind:userSelected={radioValue}/>
             </div>
 
-            <div class="pt-6 pb-3 text-lg">
-                <span class="text-lg font-semibold"> Community Tags </span>
+            <div class="pt-6 pb-3 text-sm">
+                <span class="text-sm font-semibold"> Community Tags </span>
                 <div class="flex flex-row flex-wrap w-full pt-4">
                     {#each tags as tag}
                         <!-- svelte-ignore a11y-click-events-have-key-events -->
                         <!-- svelte-ignore a11y-no-static-element-interactions -->
-                        <div class="p-2">
+                        <div class="p-1 lowercase">
                             {#if radioValue == "channel"}
                                 {#if selectedTags.indexOf(tag) == -1}
-                                    <span on:click={(e) => addRemoveTag(tag)} class="rounded-full border border-zinc-700 w-fit py-1 px-4 hover:bg-emerald-400 hover:text-black"> {tag} </span>
+                                    <span on:click={(e) => addRemoveTag(tag)} class="rounded-full border border-zinc-700 w-fit pb-1 px-3 hover:bg-emerald-400 hover:text-black"> {tag} </span>
                                 {:else}
-                                    <span on:click={(e) => addRemoveTag(tag)} class="rounded-full border border-zinc-700 w-fit py-1 px-4 bg-emerald-400 text-black"> {tag} </span>
+                                    <span on:click={(e) => addRemoveTag(tag)} class="rounded-full border border-zinc-700 w-fit pb-1 px-3 bg-emerald-400 text-black"> {tag} </span>
                                 {/if}
                             {:else}
                                 {#if selectedTags.indexOf(tag) == -1}
-                                    <span  on:click={(e) => addRemoveTag(tag)} class="rounded-full border border-zinc-700 w-fit py-1 px-4 hover:bg-cyan-400 hover:text-black"> {tag} </span>
+                                    <span  on:click={(e) => addRemoveTag(tag)} class="rounded-full border border-zinc-700 w-fit pb-1 px-3 hover:bg-cyan-400 hover:text-black"> {tag} </span>
                                 {:else}
-                                    <span  on:click={(e) => addRemoveTag(tag)} class="rounded-full border border-zinc-700 w-fit py-1 px-4 bg-cyan-400 text-black"> {tag} </span>
+                                    <span  on:click={(e) => addRemoveTag(tag)} class="rounded-full border border-zinc-700 w-fit pb-1 px-3 bg-cyan-400 text-black"> {tag} </span>
                                 {/if}
                             {/if}
                         </div>
@@ -210,8 +210,8 @@
         <!-- FOOTER -->
         <Dialog.Footer>
             <Dialog.Close>
-                <div class={radioValue == "channel" ? "rounded-full px-2 py-1 bg-white text-black hover:bg-emerald-400" : "rounded-full px-2 py-1 bg-white text-black hover:bg-cyan-400"}>
-                    <Button on:click={(e) => submitCommunity()} disabled={foundDuplicateLink} class="text-xl"> Save Changes </Button>
+                <div class={radioValue == "channel" ? "rounded-full px-2 py-0 bg-white text-black hover:bg-emerald-400" : "rounded-full px-2 py-0 bg-white text-black hover:bg-cyan-400"}>
+                    <Button on:click={(e) => submitCommunity()} disabled={foundDuplicateLink} class="text-sm font-semibold"> Save Changes </Button>
                 </div>
             </Dialog.Close>
         </Dialog.Footer>
