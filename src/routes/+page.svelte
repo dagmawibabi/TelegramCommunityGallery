b9229bddc2e8f5abe098a6797611710cee443341
bekacru <bekacru@gmail.com>
2024-05-09 20:56:28 +0300

    chore: remove bun lock file change?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -124,7 +124,7 @@
 							{#if filterFeedTags.indexOf(tag) == -1}
 								<span
 									on:click={(e) => filterFeed(tag)}
-									class="lowercase pb-1 bg-[#09090b] rounded-full border border-zinc-800 h-fit w-fit px-4 text-sm text-zinc-400 hover:bg-blue-500 hover:text-black line-clamp-1 cursor-pointer
+									class="lowercase pb-1 bg-[#09090b] rounded-full border border-zinc-800 h-fit w-fit px-4 text-sm text-zinc-400 hover:bg-blue-500 hover:text-black line-clamp-1
                                         md:text-base md:px-3
                                     "
 								>
@@ -133,7 +133,7 @@
 							{:else}
 								<span
 									on:click={(e) => filterFeed(tag)}
-									class="lowercase pb-1 bg-blue-500 rounded-full border border-zinc-800 h-fit w-fit px-3 text-base text-black font-semibold hover:text-black line-clamp-1 cursor-pointer"
+									class="lowercase pb-1 bg-blue-500 rounded-full border border-zinc-800 h-fit w-fit px-3 text-base text-black font-semibold hover:text-black line-clamp-1"
 								>
 									{tag}
 								</span>
